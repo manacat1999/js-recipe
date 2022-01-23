43e07b1056aa89fc53ad1621bd79144525107709
manacat1999 <manafee1104@gmail.com>
2022-01-23 16:06:35 +0900

    [ストップウォッチ]を学習しました。

---
+++ 02_レシピ/01_ストップウォッチ/main.js
@@ -0,0 +1,29 @@
+const display = document.getElementById("display")
+const button = document.getElementById("button")
+
+let count = 0
+
+const countUp = function() {
+  // count を更新
+  count += 1
+  console.log(count)
+  // count を秒単位にして表示
+  display.textContent = count / 100
+}
+
+
+let id = null
+
+button.onclick = function() {
+  // 10ms ごとに countUp を実行するように登録する
+  if (id === null) {
+    // start
+    id = setInterval(countUp, 10)
+    button.textContent = "stop"
+  } else {
+    // stop
+    clearInterval(id)
+    id = null
+    button.textContent = "start"
+  }
+}
