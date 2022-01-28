3bf8e817787ca89391add10b64311b103dc99e44
manacat1999 <manafee1104@gmail.com>
2022-01-28 14:39:29 +0900

    ペアセッション。

---
+++ 04_ゼミ/session/main.js
@@ -0,0 +1,15 @@
+const memoInput=document.getElementById("memo-input")
+const addButton=document.getElementById("add-button")
+const memoContainer=document.getElementById("memo-container")
+
+addButton.onclick = function() {
+    const text = memoInput.value
+    const memo = document.createElement("memo")
+    memo.className = "memo"
+    memo.textContent = text
+    
+    memoContainer.append(memo)
+
+    memoInput.value = ""
+
+}
