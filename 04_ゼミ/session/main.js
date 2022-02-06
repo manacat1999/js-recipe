809293dac22cf10ec28c59b09183c112c25dd29b
manacat1999 <manafee1104@gmail.com>
2022-02-06 18:12:08 +0900

    ぽーと

--- 04_ゼミ/session/main.js
+++ 04_ゼミ/session/main.js
@@ -3,13 +3,27 @@ const addButton=document.getElementById("add-button")
 const memoContainer=document.getElementById("memo-container")
 
 addButton.onclick = function() {
-    const text = memoInput.value
-    const memo = document.createElement("memo")
-    memo.className = "memo"
-    memo.textContent = text
-    
-    memoContainer.append(memo)
+    const card = createCard(memoInput.value)
+    memoContainer.append(card)
 
     memoInput.value = ""
-
 }
+
+const createCard = function (text) {
+    const card = document.createElement("div")
+    card.className = "card"
+    const todo = document.createElement("div")
+    todo.className = "todo"
+    todo.textContent = text
+    card.append(todo)
+  
+    const removeButton = document.createElement("button")
+    removeButton.className = "remove-button"
+    removeButton.textContent = "削除"
+    removeButton.onclick = function () {
+      card.remove()
+    }
+    card.append(removeButton)
+  
+    return card
+  }
